2fc20793e34329bc311a876943cf5a89c6dcbc3f
Aleksandar <aleksandar.temkovski@gmail.com>
2019-11-20 19:39:59 +0100

    node-mongo

--- test/index_.js
+++ test/index_.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://dev:DEV123!@cluster0-mpzfl.mongodb.net/videoteka?retryWrites=true&w=majority", {
+  .connect("mongodb+srv://<>@<>/videoteka?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })

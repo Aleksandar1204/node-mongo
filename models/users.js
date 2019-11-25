c2ffb9636f9874f054961f8b61bb8d6e93f4aac9
Aleksandar <aleksandar.temkovski@gmail.com>
2019-11-25 22:00:39 +0100

    node-mongo

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose');
 const User = mongoose.model(
     'users',
     new mongoose.Schema({
-        firs_name: String,
+        first_name: String,
         last_name:String,
         email: String,
         password: String
@@ -23,6 +23,18 @@ const createUser = (data) => {
     });
 };
 
+const getUserPasswordByEmail = (email) => {
+    return new Promise((success, fail) => {
+        User.find({email: email}, {password: 1, email: 1, first_name: 1, last_name : 1}, (err, data) => {
+            if(err){
+                return fail(err);
+            }   
+            return success(data[0]);
+        });
+    });
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUserPasswordByEmail
 }

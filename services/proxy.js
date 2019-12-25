475756ee86e6aa13f28a7ed10509cb5cb7309437
Aleksandar <aleksandar.temkovski@gmail.com>
2019-12-25 22:01:08 +0100

    change target name

--- services/proxy.js
+++ services/proxy.js
@@ -5,17 +5,17 @@ var api = express();
 var apiProxy = proxy.createProxyServer();
 
 api.all('/api/v1/auth/*', (req, res) =>{
-    apiProxy.web(req, res, {targer: 'http://localhost:8081'});
+    apiProxy.web(req, res, {target: 'http://localhost:8081'});
 
 });
 
 api.all('/api/v1/files/*', (req, res) =>{
-    apiProxy.web(req, res, {targer: 'http://localhost:8083'});
+    apiProxy.web(req, res, {target: 'http://localhost:8083'});
 
 });
 
 api.all('/api/v1/filmovi/*', (req, res) =>{
-    apiProxy.web(req, res, {targer: 'http://localhost:8080'});
+    apiProxy.web(req, res, {target: 'http://localhost:8080'});
 
 });
 
